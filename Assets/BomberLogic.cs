57210596ab4651f41e4f637ddb9cd59132907e17
AegonSnowX <156529617+AegonSnowX@users.noreply.github.com>
2024-06-10 10:43:27 -0400

    First Commit , Outlines

---
+++ Assets/BomberLogic.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberLogic : MonoBehaviour
+{
+    public float FlightSpeed = 1.2f;
+    public GameObject Bomb1Prefab;
+    public Rigidbody2D bomberRb;
+    private float bombDropInterval;
+    private float timeSinceLastBomb;
+    public GameObject Bomb2Prefab;
+    public SpriteRenderer plane;
+    private bool movingRight=true;
+
+
+
+    void Start()
+    {
+        SetRandomBombDropInterval();
+        timeSinceLastBomb = 0f;
+    }
+
+    void Update()
+    {
+        BomberMovement();
+        HandleBombDropping();
+    }
+
+    public void BomberMovement()
+    {
+        if (transform.position.x > -12 && transform.position.x < 12)
+        {
+            // Continue moving in the current direction
+            bomberRb.velocity = new Vector2(movingRight ? FlightSpeed : -FlightSpeed, bomberRb.velocity.y);
+        }
+        else
+        {
+            // Change direction when hitting the boundary
+            movingRight = !movingRight;
+            bomberRb.velocity = new Vector2(movingRight ? FlightSpeed : -FlightSpeed, bomberRb.velocity.y);
+            plane.flipX = !movingRight; // Flip the sprite to face the new direction
+        }
+    }
+
+    void HandleBombDropping()
+    {
+
+        timeSinceLastBomb += Time.deltaTime;
+        if (timeSinceLastBomb >= bombDropInterval)
+        {
+            DropBomb();
+            SetRandomBombDropInterval();
+            timeSinceLastBomb = 0f;
+        }
+    } 
+
+    void DropBomb()
+    {
+        int bombtype = Random.Range(1, 3);
+       
+
+        if ((bombtype == 1)&&(movingRight))
+        // Instantiate a bomb at the bomber's position with the bomber's rotation
+        {  
+            GameObject bomb = Instantiate(Bomb1Prefab, transform.position, Bomb1Prefab.transform.rotation);
+            SpriteRenderer bombsprite = bomb.GetComponent<SpriteRenderer>();
+
+
+
+            // Set the bomb's initial velocity to match the bomber's velocity
+            Rigidbody2D bombRb = bomb.GetComponent<Rigidbody2D>();
+            if (bombRb != null)
+            {
+                bombRb.velocity = bomberRb.velocity;
+
+            }
+        }else if((bombtype == 1) && (!movingRight))
+        {
+            GameObject bomb = Instantiate(Bomb1Prefab, transform.position, Bomb1Prefab.transform.rotation);
+            SpriteRenderer bombsprite = bomb.GetComponent<SpriteRenderer>();
+
+
+
+            // Set the bomb's initial velocity to match the bomber's velocity
+            Rigidbody2D bombRb = bomb.GetComponent<Rigidbody2D>();
+            if (bombRb != null)
+            {
+                bombRb.velocity = bomberRb.velocity;
+
+            }
+        }
+
+
+
+        else
+
+        {
+            GameObject bomb = Instantiate(Bomb2Prefab, transform.position, Bomb2Prefab.transform.rotation);
+
+            // Set the bomb's initial velocity to match the bomber's velocity
+            Rigidbody2D bombRb = bomb.GetComponent<Rigidbody2D>();
+            if (bombRb != null)
+            {
+                bombRb.velocity = bomberRb.velocity;
+
+            }
+        }
+
+
+    }
+
+    
+
+    void SetRandomBombDropInterval()
+    {
+        // Set a random interval between 1 and 8 seconds
+        bombDropInterval = Random.Range(1f, 8f);
+    }
+}
---
+++ Assets/VehicleMovement.cs
@@ -0,0 +1,64 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VehicleMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    private Vector2 moveVelocity;
+    public Rigidbody2D rb;
+    public SpriteRenderer truck;
+    private float lastMoveX = 1f;
+
+    public GameObject turret; // Reference to the turret
+    public float turretOffsetXRight = -3.9f; // Adjust this value for right facing
+    public float turretOffsetXLeft = 3.9f; // Adjust this value for left facing
+
+    void Start()
+    {
+        // Store the initial offset of the turret from the truck
+    }
+
+    void Update()
+    {
+        TruckMovement();
+    }
+
+    public void TruckMovement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        moveVelocity = new Vector2(moveX * MovementSpeed, rb.velocity.y);
+
+        if (moveX != 0)
+        {
+            lastMoveX = moveX;
+        }
+
+        // Flip the sprite based on the last movement direction
+        if (lastMoveX > 0)
+        {
+            truck.flipX = true; // Facing right
+            AdjustTurretPosition(true); // Adjust turret position when facing right
+        }
+        else if (lastMoveX < 0)
+        {
+            truck.flipX = false; // Facing left
+            AdjustTurretPosition(false); // Adjust turret position when facing left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+    }
+
+    void AdjustTurretPosition(bool facingRight)
+    {
+        // Adjust the turret's local position to remain at the back of the truck
+        float turretOffsetX = facingRight ? turretOffsetXRight : turretOffsetXLeft;
+        turret.transform.localPosition = new Vector3(turretOffsetX, turret.transform.localPosition.y, turret.transform.localPosition.z);
+    }
+}
