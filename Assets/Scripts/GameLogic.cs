defe35db301cf06ec8c480d70096cd51c1bfb7ad
MarsPH <m.poorhamidian@gmail.com>
2024-07-15 15:28:09 -0400

    Bug Fixed: The loop of spawn works fine

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -6,38 +6,36 @@ using UnityEngine;
 public class Gamelogic : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI wavetext;
-    [SerializeField] int wave = 0;
+    [SerializeField] int wave = 1;
     public GameObject Plane;
 
      void Start()
     {
 
     }
-     void Update()
+     void LateUpdate()
     {
         if (GameObject.FindGameObjectWithTag("Enemy Plane") == null)
         {
             Wave();
         }
         
-        
-        else
-        {
-            Debug.Log("Wave Not Called");
-        }
-
     }
     public void Wave()
     {
-       
+            for (int i = 0; i < wave; i++)
+        {
+            float loopCap = 0;
             float x = Random.Range(12.0f, 14.0f);
             float y = Random.Range(1.50f, 3.60f);
             Instantiate(Plane, new Vector2(-x, y), Quaternion.Euler(180f, 0f, 180f));
-            wave++;
             wavetext.text = "Wave: " + (wave - 1);
             Debug.Log("Spawn");
-        
-        
+            loopCap++;
+
+        }
+        wave++;
+
 
     }
 }
