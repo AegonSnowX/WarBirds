5fa89038e19cd666f4ed48b1c72905f9513b196c
AegonSnowX <156529617+AegonSnowX@users.noreply.github.com>
2024-07-22 21:52:18 -0400

    Minor Bug Fixes

--- Assets/Scripts/BulletLogic.cs
+++ Assets/Scripts/BulletLogic.cs
@@ -1,39 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class AntiAirRocketExplosion : MonoBehaviour
-{
- 
-    [SerializeField] GameObject ExplosionOfPlanes;
-    private Transform enemy;
-    
 
+public class BulletLogic : MonoBehaviour
+{
+    [SerializeField] GameObject explosive;
     // Start is called before the first frame update
     void Start()
     {
-      
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-
-            Destroy(gameObject);
-            Destroy(collision.gameObject);
-        enemy = collision.gameObject.GetComponent<Transform>();
-        GameObject explosion = Instantiate(ExplosionOfPlanes, enemy.transform.position, Quaternion.identity);
+       Destroy(gameObject);
+       GameObject explosion = Instantiate(explosive,transform.position, Quaternion.identity) ;
         ExplosionDestroyer destroyer = explosion.AddComponent<ExplosionDestroyer>();
         destroyer.lifeSecondsLeft = 4;
         Destroy(gameObject);
-
-
     }
 }
-
