5d9718b508276416c9b4b6842cb8e5d9f04f821d
MarsPH <m.poorhamidian@gmail.com>
2024-07-28 11:24:14 -0400

    Bug Fixed: Border Matching Velocity

--- Assets/Scripts/FlipWhenTriggered.cs
+++ Assets/Scripts/FlipWhenTriggered.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class FlipWhenTriggered : MonoBehaviour
 {
     public string TriggerObjectName = "BorderLeft";
+    private Dictionary<GameObject, Vector2> originalVelocities = new Dictionary<GameObject, Vector2>();
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy Plane"))
         {
             Rigidbody2D collisionRb = collision.gameObject.GetComponent<Rigidbody2D>();
-            
+
+            if (!originalVelocities.ContainsKey(collision.gameObject))
+            {
+                originalVelocities[collision.gameObject] = collisionRb.velocity;
+            }
+
+
             GameManager.Instance.Flip(collision.gameObject);
+
+            Vector2 originalVelocity = originalVelocities[collision.gameObject];
+
             if (gameObject.name == TriggerObjectName)
-                collisionRb.velocity = collision.gameObject.transform.TransformDirection(Vector2.right);
+            {
+                collisionRb.velocity = collision.gameObject.transform.TransformDirection(Vector2.right) * originalVelocity.magnitude; 
+            }
+             
             else
-                collisionRb.velocity = collision.gameObject.transform.TransformDirection(Vector2.left);
+            {
+                collisionRb.velocity = collision.gameObject.transform.TransformDirection(Vector2.left) * originalVelocity.magnitude;
+            }
+
         }
 
     }
